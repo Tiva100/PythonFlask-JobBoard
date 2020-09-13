d010933481cf0bb8922261397489a8c9b5afbfba
vivek antony <vivekantony777@gmail.com>
2020-09-13 10:00:29 +0530

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -42,4 +42,5 @@ def job(job_id):
 
 @app.route('/employer/<employer_id>')
 def employer(employer_id):
-    return render_template('employer.html')
+    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
+    return render_template('employer.html', employer=employer)
